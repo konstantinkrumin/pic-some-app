f3268c050444bf70d4179f8f9a96b2529023d7cc
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-12 01:24:49 +0300

    Updates

---
+++ precache-manifest.95f92268cab2fd1d32d950a0c0b05ba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd116efd65f3876da583797a1b0ce803",
+    "url": "/pic-some-app/index.html"
+  },
+  {
+    "revision": "386230b86cbd07ecbece",
+    "url": "/pic-some-app/static/css/main.abe5327b.chunk.css"
+  },
+  {
+    "revision": "f7af7cfa3a338f2a508e",
+    "url": "/pic-some-app/static/js/2.6c52e7f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pic-some-app/static/js/2.6c52e7f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "386230b86cbd07ecbece",
+    "url": "/pic-some-app/static/js/main.df5f6747.chunk.js"
+  },
+  {
+    "revision": "59dc57b9fe9be8165d69",
+    "url": "/pic-some-app/static/js/runtime-main.c6532be5.js"
+  }
+]);
